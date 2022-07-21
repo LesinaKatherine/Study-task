712d89750fdd8d534485a5a00c051a09af784f1b
LesinaKatherine <109028781+LesinaKatherine@users.noreply.github.com>
2022-07-21 20:38:07 +0300

    Update lesson 18_debug.py

--- lesson 18_debug.py
+++ lesson 18_debug.py
@@ -2,14 +2,14 @@ def debug(func):
     def wrapper(*args):
         print(f'Имя функции: {func.__name__}, значения: {args}')
         print('Результат выполнения  функции')
-        func()
+        func(*args)
 
     return wrapper
 
 
 @debug
-def ex(*args):
-    print(sum(args))
+def ex(a,b):
+    print(a + b)
 
 
 ex(2, 4)
